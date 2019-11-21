070e9a6da1887044c9c0e014cf0cd3225facb120
helloalexdodd <alexdodd@gmx.com>
2019-11-21 13:55:47 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Todo({ todo, i }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="todo">{ todo.text }</div>
+  )
 }
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        className="input"
+        value={value} 
+        placeholder="add new to do"
+        onChange={e => setValue(e.target.value)} 
+      />
+    </form>
+  )
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn Hooks',
+      isCompleted: false
+    },
+    {
+      text: 'Appy hooks to Boggle',
+      isCompleted: false
+    },
+    {
+      text: 'Get coffee',
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo}/>
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  )
+};
 
 export default App;
